72921423b7d0610c627b08a0822ca46febe790ba
oceanianzs <oceanianzs@gmail.com>
2023-11-12 17:07:08 +0300

    Edit main.py

--- main.py
+++ main.py
@@ -1,32 +1,39 @@
 import random
 
 def guess_number_game():
-    # Generate a random number between 1 and 10
+    # this generates a random number between like 1 and 10 wowzers  
     secret_number = random.randint(1, 10)
     
     attempts = 0
-    max_attempts = 3  # You can adjust the number of attempts as needed
+    max_attempts = 3  
+    invalid_input_count = 0
+    max_invalid_inputs = 3  
     
-    print("Welcome to the Number Guessing Game!")
-    print("I have chosen a number between 1 and 10. Can you guess it?")
+    print("welcome, i chose a number between 1 and 10. try to guess it")
     
     while attempts < max_attempts:
         try:
-            # Get the user's guess
-            guess = int(input("Enter your guess: "))
+            # this gets users guess
+            guess = int(input("input your guess: "))
             
-            # Check if the guess is correct
+            # is the guess correct or not
             if guess == secret_number:
-                print("Congratulations! You guessed the correct number.")
+                print("hey good job")
                 break
             else:
-                print("Sorry, that's not the correct number. Try again.")
+                print("not correct, try again.")
                 attempts += 1
         except ValueError:
-            print("Invalid input. Please enter a valid number.")
+            invalid_input_count += 1
+            print("thats not a correct input, only input numbers.")
+            
+            # checks if the player reached max invalid inputs
+            if invalid_input_count == max_invalid_inputs:
+                print(f"the game ended because you put too much invalid inputs.. what the actual hell")
+                break
     
     if attempts == max_attempts:
-        print(f"Sorry, you've run out of attempts. The correct number was {secret_number}.")
+        print(f"sorry. you ran outta attempts!! the correct number was {secret_number}.")
 
 # Run the game
 guess_number_game()
